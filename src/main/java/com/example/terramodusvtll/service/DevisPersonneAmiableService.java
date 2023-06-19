9bc2c4f5dd60a8adfc5daac30b637ebbca407474
ELHAIRECH Saifeddine <72352913+ELHAIRECH@users.noreply.github.com>
2023-06-19 15:13:39 +0100

    Update DevisPersonneAmiableService.java

--- src/main/java/com/example/terramodusvtll/service/DevisPersonneAmiableService.java
+++ src/main/java/com/example/terramodusvtll/service/DevisPersonneAmiableService.java
@@ -1092,7 +1092,7 @@ public class DevisPersonneAmiableService {
         document.close();
         outputStream1.close();
         File file = new File("src/main/resources/exported_report.docx");
-        emailService.sendHtmlMessage("anassbougaterra@gmail.com", "Demande Devis", byteArray, file);
+        emailService.sendHtmlMessage("ouadi3laila@gmail.com", "Demande Devis", byteArray, file);
         return savedDevis;
         }
         public List<DevisPersonneAmiable> findAll() {
